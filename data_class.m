a9d8704210d3f3b9430cf3297dec52cddae4b84f
Kaixiang Zhuang <oldzhuang@outlook.com>
2023-10-20 16:03:25 +0800

    Update data_class.m

--- data_class.m
+++ data_class.m
@@ -1,42 +1,36 @@
-function [cgroup] = data_class(group,class,v)
+function [cgroup] = data_class(value,class,v)
 %% Data Classification
 %% -------------------------------------------------------------------------------------------------------------------------------
 %% Input
-%  (1) group: classification basis (double array data for categorical or continuous variables)
+%  (1) value: classification basis (double array data for categorical or continuous variables)
 %  (2) class: classification method ('cat'\'bin','window')
-%  (3) v: for "class = bin", v should include edges or boundaries in bins;
+%  (3) v: for "class = bin", v should be the number bins;
 %         for "class = window", v(1) is the length and v(2) is the step width of windows
 %% Output
 %  (1) cgroup: Data Classification ID 
 %% Example
-%  cgroup = data_class([1:100],'window',[10 2]); % behavior-identified sliding windows
+%  cgroup = data_class([1:100],'window',[10 2]);
 %% -------------------------------------------------------------------------------------------------------------------------------
 % - Z.K.X. 2021/06/04 (MATLAB R2018a)
 %% -------------------------------------------------------------------------------------------------------------------------------
 cgroup = [];
 if strcmp(class,'cat')
-   g = unique(group);
+   g = unique(value);
    for i = 1:length(g)
-       f = find(group==g(i));
+       f = find(value==g(i));
        cgroup = [cgroup;f];
    end
-   group_info = [];
 elseif strcmp(class,'bin') 
-   [B,E] = discretize(group,v);
-   g = unique(B);
-   for i = 1:length(g)
-       f = find(B==g(i));
-       cgroup = [cgroup;f];
-   end
+    edges = linspace(min(value), max(value), v+1);
+    cgroup = discretize(value, edges);
 elseif strcmp(class,'window') 
-   g = unique(group);
+   g = unique(value);
    g = sort(g);
    P = sliding_window(g,v(1),v(2));
    for i = 1:size(P,1)
        f = P(i,:);
        cgroup = [cgroup;f];
    end  
-  group_info = [];
 end
 
 end
